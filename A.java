caef994089d143f1c1f6a5bd7b67b8a30ef93e9d
Rodolpho Traboussy <rtraboussy@computacao.ufla.br>
2018-10-23 19:04:05 -0300

    Adição do atributo A3

--- A.java
+++ A.java
@@ -1,6 +1,7 @@
 public class A{
     private int A1;
     private float A2;
+    private String A3;
     
     public void setA1(int a1){
         A1 = a1;
@@ -10,11 +11,19 @@ public class A{
         A2 = a2;
     }
     
+    public void setA3(String A3){
+        A3 = a3;
+    }
+    
     public int getA1(){
         return A1;
     }
     
     public float getA2(){
         return A2;
+    }
+    
+    public String getA3(){
+        return A3;
     }
 }
