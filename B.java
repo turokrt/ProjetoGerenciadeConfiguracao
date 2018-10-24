7dd9fdef1aaa25f371f14e0b83b2560a764c6d72
Rodolpho Traboussy <rtraboussy@computacao.ufla.br>
2018-10-24 18:28:18 -0300

    Update B.java

--- B.java
+++ B.java
@@ -1,6 +1,7 @@
 public class B{
     private int B1;
     private float B2;
+    private String B4;
     
     public void setB1(int b1){
         B1 = b1;
@@ -10,11 +11,19 @@ public class B{
         B2 = b2;
     }
     
+    public void setB4(String b4){
+        B4 = b4;
+    }
+    
     public int getB1(){
         return B1;
     }
     
     public float getB2(){
         return B2;
+    }
+    
+    public String getB4(){
+        return B4;
     }
 }
